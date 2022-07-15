33f4ca74b8877ae93d1297d4e7503a91140a8145
OfficialAhmed <Ahmed.riyami1818@gmail.com>
2022-07-15 19:19:51 +0400

    update

--- MyTrophies.py
+++ MyTrophies.py
@@ -1,4 +1,5 @@
 from ftplib import FTP
+from tkinter.messagebox import RETRY
 import eel
 
 eel.init('UI')
@@ -6,11 +7,21 @@ eel.init('UI')
 class My_trophies:
     def __init__(self):
         self.ftp = FTP()
+        self.bronze_credit = 15
+        self.silver_credit = 30
+        self.gold_credit = 90
+        self.plat_credit = 300
+        self.users_dir = ""
+        self.trophy_dir = ""
         self.ip = ""
         self.port = 0
-        self.dir = ""
 
-    def connect(self, ip: str, port: int):
+    def connect(self, ip: str, port: int) -> tuple:
+        """
+        #############################
+            Init PS4 connection
+        #############################
+        """
         try: 
             self.ip = ip
             self.port = port
@@ -23,39 +34,149 @@ class My_trophies:
             error = str(e)
             return (False, error)
 
-    def login(self):
+    def login(self) -> None:
+        """
+        ################################
+            Login to and fetch users
+        ################################
+        """
         self.ftp.login("", "")
         if self.ftp.getwelcome():
-            pass
+            print("Connected!")
 
-    def calculate_points(self, trophies):
-        bronze_credit = 15
-        silver_credit = 30
-        gold_credit = 90
-        plat_credit = 300
-
+    def get_points(self, trophies: list) -> int:
+        """
+        ##################################################
+            Calculate trophy points from (param:trophies)
+        ##################################################
+        """
         b, s, g, p = trophies
 
-        b *= bronze_credit
-        s *= silver_credit
-        g *= gold_credit
-        p *= plat_credit
+        b *= self.bronze_credit
+        s *= self.silver_credit
+        g *= self.gold_credit
+        p *= self.plat_credit
 
         return b+s+g+p
         
+    def get_all_trophies(self) -> list:
+        """
+        ############################################
+            Fetch user trophy data from trophy file
+        ############################################
+        """
+
+        self.bronze = 936
+        self.silver = 331
+        self.gold = 122
+        self.plat = 22
+        total = self.bronze + self.silver + self.gold + self.plat
+        return [self.bronze, self.silver, self.gold, self.plat, total]
+
+    def get_user_info(self) -> tuple:
+        def get_level_and_percent(points: int) -> int:
+            l100 = 5940 
+            l200 = 9000 
+            l300 = 45000 
+            l400 = 90000 
+            l500 = 135000
+            l600 = 180000
+            l700 = 225000
+            l800 = 270000
+            l900 = 315000
+            
+            # how many points to level up, each level require 
+            if points <= l100:
+                multiplier = 100
+                level_points = l100
+            elif points <= l200:
+                multiplier = 200 
+                level_points = l200
+            elif points <= l300:
+                multiplier = 300 
+                level_points = l300
+            elif points <= l400:
+                multiplier = 400 
+                level_points = l400
+            elif points <= l500:
+                multiplier = 500
+                level_points = l500
+            elif points <= l600:
+                multiplier = 600
+                level_points = l600
+            elif points <= l700:
+                multiplier = 700
+                level_points = l700
+            elif points <= l800:
+                multiplier = 800
+                level_points = l800
+            elif points <= l900:
+                multiplier = 900
+                level_points = l900
+            else:
+                multiplier = 1000
+                level_points = 360,000
+
+            count = (points / level_points) * multiplier
+            level = int(count)
+            percentage = int((count - level) * 100)
+
+            return (level, percentage)
+
+        def get_icon(level: int) -> str:
+            if level >= 1 and level <= 299:
+                return "Bronze"
+            elif level >= 300 and level <= 599:
+                return "Silver"
+            elif level >= 600 and level <= 998:
+                return "Gold"
+            else:
+                return "Platinum"
+
+        points = self.get_points([
+            self.bronze,
+            self.silver,
+            self.gold,
+            self.plat
+        ])
+
+        level_and_percentage = get_level_and_percent(points)
+        level, percentage = level_and_percentage
+        icon = get_icon(level)
+
+        return (level, percentage, icon)
+
+    def get_trophies_to_levelup(self):
+        pass
+    
 ps4 = My_trophies()
 
 @eel.expose
-def init_connection(ip: str, port: str):
+def init_connection(ip: str, port: str) -> tuple:
     global ps4
-    respond = ps4.connect(ip, int(port.strip()))
-    return respond    
+    return ps4.connect(ip, int(port.strip()))    
+
+@eel.expose
+def get_points(trophies: list):
+    global ps4
+    return ps4.get_points(trophies)
+
+@eel.expose
+def get_all_trophies():
+    global ps4
+    return ps4.get_all_trophies()
+
+@eel.expose
+def get_user_info():
+    global ps4
+    return ps4.get_user_info()
 
 
 @eel.expose
-def calculate(trophies):
+def get_trophies_to_levelup(): #unimplemented
     global ps4
-    return ps4.calculate_points(trophies)
+    return ps4.get_trophies_to_levelup()
+    
 
 @eel.expose
 def get_app_ver():
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,47 +1,80 @@
 async function connect_pressed() {
-    /////////////////////////////////
-    //   Check PS4 FTP connection
-    /////////////////////////////////
+  /////////////////////////////////
+  //   Check PS4 FTP connection
+  /////////////////////////////////
 
-    let status = document.getElementById("wb_uid1");
-    let ip = document.getElementById("ip_field").value;
-    let port = document.getElementById("port_field").value;
+  let status = document.getElementById("wb_uid1");
+  let ip = document.getElementById("ip_field").value;
+  let port = document.getElementById("port_field").value;
 
-    status.innerHTML = "CONNECTING...";
-    let respond = await eel.init_connection(ip, port)();
-    if (respond[0] == false) {
-        alert(respond[1]);
-        status.innerHTML = "FAILED TO CONNECT";
-        status.style.color = "red";
-    } else {
-        status.innerHTML = "CONNECTED";
-        status.style.color = "green";
-    }
-    status.style.fontSize = "15px";
+  status.innerHTML = "CONNECTING...";
+  let respond = await eel.init_connection(ip, port)();
+  if (respond[0] == false) {
+    alert(respond[1]);
+    status.innerHTML = "FAILED TO CONNECT";
+    status.style.color = "red";
+  } else {
+    status.innerHTML = "CONNECTED";
+    status.style.color = "green";
+  }
+  status.style.fontSize = "15px";
 }
 
 async function calculate_points() {
-    /////////////////////////////////////////////////////
-    //   calculate trophy points any time user onchange
-    /////////////////////////////////////////////////////
-    let bronze = parseInt(document.getElementById("user_bronze").value);
-    let silver = parseInt(document.getElementById("user_silver").value);
-    let gold = parseInt(document.getElementById("user_gold").value);
-    let plat = parseInt(document.getElementById("user_plat").value);
+  /////////////////////////////////////////////////////
+  //   calculate trophy points any time user onchange
+  /////////////////////////////////////////////////////
 
-    trophies = [bronze, silver, gold, plat];
+  let bronze = parseInt(document.getElementById("user_bronze").value);
+  let silver = parseInt(document.getElementById("user_silver").value);
+  let gold = parseInt(document.getElementById("user_gold").value);
+  let plat = parseInt(document.getElementById("user_plat").value);
 
-    for (let index = 0; index < trophies.length; index++) {
-        if (isNaN(trophies[index])) {
-            trophies[index] = 0;
-        }
+  trophies = [bronze, silver, gold, plat];
+
+  for (let index = 0; index < trophies.length; index++) {
+    if (isNaN(trophies[index])) {
+      trophies[index] = 0;
     }
+  }
 
-    // wait for Python to return the result
-    result = await eel.calculate(trophies)();
-    document.getElementById("calculator_result").value = result;
+  // wait for Python to return the result
+  result = await eel.get_points(trophies)();
+  document.getElementById("calculator_result").value = result;
 }
 
+async function generate_data() {
+  let userName = document.getElementById("users").value;
+
+  let bronze = document.getElementById("total_bronze");
+  let silver = document.getElementById("total_silver");
+  let gold = document.getElementById("total_gold");
+  let plat = document.getElementById("total_plat");
+  let total = document.getElementById("total_trophies");
+
+  let trophy_obj = [bronze, silver, gold, plat, total];
+  let trophies = await eel.get_all_trophies()();
+
+  for (let index = 0; index < trophies.length; index++) {
+    trophy_obj[index].value = trophies[index];
+  }
+
+  let user_info = await eel.get_user_info()();
+  let level = user_info[0];
+  let percent = user_info[1];
+  let percent_color = 100 - percent;
+  let icon = user_info[2];
+
+  document.getElementById("wb_uid8").innerHTML = level + "";
+  document.getElementById("level_percentage-label").innerHTML = percent + "%";
+
+  document.getElementById("level_percentage").style.backgroundImage =
+    "linear-gradient( to left, white " +
+    percent_color +
+    "%, rgba(51, 147, 250, 0.845) 100%)";
+}
+
+<<<<<<< Updated upstream
 // window.onload = function() {
 //     app_version = 3.04;
 //     document.title = "My Trophies v" + app_version;
@@ -108,12 +141,77 @@ $(document).ready(function() {
         value: 0,
         change: function() {
             $("#level_percentage-label").text($(this).progressbar("value") + "%");
+=======
+$(document).ready(function () {
+  $("a[href*='#trophies_layout_grid']").click(function (event) {
+    event.preventDefault();
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $("#wb_trophies_layout_grid").offset().top - 88,
+>>>>>>> Stashed changes
         },
-    });
-    $("#save_btn").button({
-        icon: "ui-primary",
-        iconPosition: "beginning",
-    });
-    $("#fetch_btn").button();
-    $("#connect_btn").button();
+        600,
+        "easeOutCirc"
+      );
+  });
+  $("#ip_field").validate({
+    required: true,
+    bootstrap: true,
+    type: "custom",
+    param:
+      /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
+    length_min: "4",
+    color_text: "#000000",
+    color_hint: "#00FF00",
+    color_error: "#FF0000",
+    color_border: "#808080",
+    nohint: false,
+    font_family: "Arial",
+    font_size: "13px",
+    position: "topleft",
+    offsetx: 0,
+    offsety: 0,
+    effect: "none",
+    error_text: "Invalid IP",
+  });
+  $("#ip_field").inputmask({
+    alias: "ip",
+  });
+  $("#port_field").validate({
+    required: true,
+    bootstrap: true,
+    type: "number",
+    expr_min: ">=",
+    expr_max: "",
+    value_min: "0",
+    value_max: "",
+    length_min: "1",
+    length_max: "8",
+    color_text: "#000000",
+    color_hint: "#00FF00",
+    color_error: "#FF0000",
+    color_border: "#808080",
+    nohint: false,
+    font_family: "Arial",
+    font_size: "13px",
+    position: "topleft",
+    offsetx: 0,
+    offsety: 0,
+    effect: "none",
+    error_text: "Invalid Port",
+  });
+  $("#level_percentage").progressbar({
+    value: 0,
+    change: function () {
+      $("#level_percentage-label").text($(this).progressbar("value") + "%");
+    },
+  });
+  $("#save_btn").button({
+    icon: "ui-primary",
+    iconPosition: "beginning",
+  });
+  $("#fetch_btn").button();
+  $("#connect_btn").button();
 });
